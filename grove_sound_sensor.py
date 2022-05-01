d44687ef36ff18f5296d801eb551cb1aefda22bc
pkowshik2020 <pkowshik@usc.edu>
2022-04-30 18:38:34 -0700

    twill

--- grove_sound_sensor.py
+++ grove_sound_sensor.py
@@ -42,13 +42,13 @@ import time
 import grovepi
 from twilio.rest import Client
 
-'''
+
 #Send an SMS message to the client's phone number through the number registered with twilio
 TWILIO_ACCOUNT_SID = 'AC1ae904bb7ab7a6b87f6587b712b45657' # replace with your Account SID
-TWILIO_AUTH_TOKEN = 'acdc68d0a79a549f590b1d183a22188d' # replace with your Auth Token
+TWILIO_AUTH_TOKEN = 'cd080981ea88a3291fdbcf2cf762c511' # replace with your Auth Token
 TWILIO_PHONE_SENDER = '+17853845784' # replace with the phone number you registered in twilio
 TWILIO_PHONE_RECIPIENT = '+17074943499' # replace with your phone number
-'''
+
 # Connect the Grove Sound Sensor to analog port A0
 # SIG,NC,VCC,GND
 sound_sensor = 0
@@ -65,14 +65,14 @@ raw_data = []
 averaged_data = []
 num_points = 0
 
-#def send_text_alert(alert_str):
-#    """Sends an SMS text alert."""
-#    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-#    message = client.messages.create(
-#        to=TWILIO_PHONE_RECIPIENT,
-#        from_=TWILIO_PHONE_SENDER,
-#        body='Knocking sequence failed! Beware of potential intruders.')
-#    print(message.sid)
+def send_text_alert(alert_str):
+    """Sends an SMS text alert."""
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+    message = client.messages.create(
+        to=TWILIO_PHONE_RECIPIENT,
+        from_=TWILIO_PHONE_SENDER,
+        body='Knocking sequence failed! Beware of potential intruders.')
+    print(message.sid)
 '''
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 message = client.messages.create(
